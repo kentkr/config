58eecf110ba92f288a4de31291db8dafcc818eff
Kyle Kent <kyle.kent321@gmail.com>
2024-10-25 10:58:35 -0500

    vim-plug install

--- configure_nvim.sh
+++ configure_nvim.sh
@@ -2,3 +2,7 @@
 
 # neovim setup
 cp -r ./nvim ~/.config/
+
+# vim-plug install
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
